e2c3bfa5a7b6ccf88a42b3e8adb020ba65383964
anikap1234 <136335913+anikap1234@users.noreply.github.com>
2024-06-29 11:58:55 +0530

    Update solar.py

--- solar.py
+++ solar.py
@@ -3,7 +3,7 @@ import math
 pygame.init()
 
 WIDTH, HEIGHT =  800, 800
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+WIN = pygame.display.set_mode((WIDTH, HEIGHT)) #window
 pygame.display.set_caption("Planet Simulation")
 
 WHITE = (255, 255, 255)
@@ -15,8 +15,8 @@ DARK_GREY = (80, 78, 81)
 FONT = pygame.font.SysFont("comicsans", 16)
 
 class Planet:
-	AU = 149.6e6 * 1000
-	G = 6.67428e-11
+	AU = 149.6e6 * 1000 #distance from the sun 
+	G = 6.67428e-11 
 	SCALE = 250 / AU  # 1AU = 100 pixels
 	TIMESTEP = 3600*24 # 1 day
 
@@ -61,7 +61,7 @@ class Planet:
 		distance = math.sqrt(distance_x ** 2 + distance_y ** 2)
 
 		if other.sun:
-			self.distance_to_sun = distance
+			self.distance_to_sun = distance #if other object is sun 
 
 		force = self.G * self.mass * other.mass / distance**2
 		theta = math.atan2(distance_y, distance_x)
@@ -110,18 +110,18 @@ def main():
 	planets = [sun, earth, mars, mercury, venus]
 
 	while run:
-		clock.tick(60)
+		clock.tick(60)   #updating 60 times per second
 		WIN.fill((0, 0, 0))
 
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
+		for event in pygame.event.get():    #looping through the events 
+			if event.type == pygame.QUIT:  #if window closes 
 				run = False
 
 		for planet in planets:
 			planet.update_position(planets)
 			planet.draw(WIN)
 
-		pygame.display.update()
+		pygame.display.update() #update display with every loop
 
 	pygame.quit()
 
